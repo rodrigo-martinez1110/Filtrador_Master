3b63089022adfe25726ad733f9ea284c1200b060
Rodrigo Martinez <jorbinaldo@gmail.com>
2025-05-07 13:45:08 -0300

    Update filtro_master.py

--- filtro_master.py
+++ filtro_master.py
@@ -84,11 +84,13 @@ st.sidebar.subheader("📂 Arquivos com Margem")
 arquivo_novo = st.sidebar.file_uploader("Arquivo com margem (opcional)", type="csv", accept_multiple_files=True)
 
 st.sidebar.subheader("Filtros")
-apenas_saque_complementar = st.sidebar.checkbox("Saldo Devedor maior que 0")
-equipes_konsi = ['outbound', 'csapp', 'csport', 'cscdx', 'csativacao', 'cscp']
-equipe = st.sidebar.selectbox("Selecione a Equipe", equipes_konsi)
-comissao_banco = st.sidebar.number_input("Comissão do banco (%): ", value=0.00) / 100
-comissao_minima = st.sidebar.number_input("Comissão mínima: ", value=0.0)
+with st.sidebar.expander("Filtradores"):
+    apenas_saque_complementar = st.checkbox("Saldo Devedor maior que 0")
+    equipes_konsi = ['outbound', 'csapp', 'csport', 'cscdx', 'csativacao', 'cscp']
+    equipe = selectbox("Selecione a Equipe", equipes_konsi)
+    comissao_banco = number_input("Comissão do banco (%): ", value=0.00) / 100
+    comissao_minima = number_input("Comissão mínima: ", value=0.0)
+    
 
 # Processamento principal
 base_final = pd.DataFrame()
@@ -127,6 +129,7 @@ if not base_final.empty:
             base_final[col] = None
 
     base_final = base_final[colunas_finais]
+    base['banco_beneficio'] = '243'
     data_hoje = datetime.today().strftime('%d%m%Y')
     base_final['Campanha'] = base_final['Convenio'].str.lower() + '_' + data_hoje + '_benef_' + equipe
     base_final['comissao_beneficio'] = (base_final['valor_liberado_beneficio'] * comissao_banco).round(2)
